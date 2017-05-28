09a6bf04705e7c06f3a46827f3a0d6e02d96d9db
Davin <davin.shc@gmail.com>
2017-05-27 20:35:45 -0400

    first for design portfoio

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
+    
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy({scrollOffset: 0});
+    $('.materialboxed').materialbox();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
